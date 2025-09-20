781b43ba83350e34c8c2c481416b9900960b5a2e
Kundan Kumar <kk5691337@gmail.com>
2025-09-20 20:12:31 +0000

    compair

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,7 +1,7 @@
 let score = "33";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 // console.log(typeof valueInNumber);
@@ -9,5 +9,48 @@ let valueInNumber = Number(score)
 let someNumber = 44
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// *************************** Operation ******************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+
+let str1 = "kundan"
+let str2 = "kumar"
+
+let str3 = str1+str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("2" + 1);
+// console.log("1" + 2*2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+// console.log(gameCounter);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
