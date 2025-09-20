ed7f1fd405e492bf41fa052a0add9306cd992617
Kundan Kumar <kk5691337@gmail.com>
2025-09-20 19:12:46 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "kund@goolgle.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hcfd@c.com"
+accountPassword = "324536456"
+accountCity = "gohitie"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict";
+
+console.log(3
+    +3
+)
+
+console.log("kundan")
+
+let name = "kundan"
+let age  = 23
+let isLoggedIn = false
