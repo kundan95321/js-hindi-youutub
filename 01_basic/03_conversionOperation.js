4f7e7855b38ca2901228e59ab37f99bf12fb6897
Kundan Kumar <kk5691337@gmail.com>
2025-09-20 19:44:50 +0000

    conversion of dataType

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "33";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+let someNumber = 44
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
