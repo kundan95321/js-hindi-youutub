e9e363fc7f485153b0e96162e409a230ce08028f
Kundan Kumar <kk5691337@gmail.com>
2025-09-21 17:40:23 +0000

    07_datasinJs.js

---
+++ 01_basic/07_datasinJs.js
@@ -0,0 +1,15 @@
+// data
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof moveBy);
+
+let myCreatedDate = new Date(2023, 0, 23)
+// console.log(myCreatedDate.toDateString());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
