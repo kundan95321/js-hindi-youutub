597d304dec732e366ae82f5ffec730356a8a54e9
Kundan Kumar <kk5691337@gmail.com>
2025-09-21 10:57:37 +0000

    dataType-summary

---
+++ 01_basic/dataTypes-Summary.js
@@ -0,0 +1,17 @@
+let myYoutubename = "kundankumar"
+
+let anothername = myYoutubename
+anothername = "college"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "usgfrrdgf@google.com",
+    upi: "usert@ybl"
+}
+
+let userTwo = userOne
+
+console.log(userOne.email);
+console.log(userTwo.email);
