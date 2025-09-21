33a89dbae10d2c61a0b18d5a8e9322de93d36704
Kundan Kumar <kk5691337@gmail.com>
2025-09-21 11:56:13 +0000

    math and nums

---
+++ 01_basic/06_nums_math.js
@@ -0,0 +1,20 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.9876
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+// ********************************** Maths *****************
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
